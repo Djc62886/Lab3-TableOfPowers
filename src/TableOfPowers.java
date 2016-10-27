fca3da54873d8a8a201604bbbb7bf0058cce1183
Daniel Christiansen <dchristiansen40@gmail.com>
2016-10-27 10:23:51 -0400

    first commit

---
+++ src/TableOfPowers.java
@@ -0,0 +1,54 @@
+
+/* Lab 3 Task: Pair program and display a TABLE OF POWERS. 
+ * Prompt the user to enter an integer. 
+ * Display a TABLE of SQUARES and CUBES from 1 to the value entered by user.
+ * Ask if the user wants to continue. (y/n)? 
+ * Assume that the user will enter valid data. 
+ * Only continue if the user agrees to. 
+ * @author DanielChristiansen 
+ * @author ClaudiaZavala
+ */
+
+import java.util.Scanner;
+
+public class TableOfPowers {
+	public static void main(String[] args) {
+
+		System.out.println("Learn your squares and cubes!");
+		System.out.println();
+
+		Scanner sc = new Scanner(System.in);
+		String choice = "y";
+
+		do {
+			System.out.print("Enter an integer: ");
+			int integer = sc.nextInt();
+
+			String columns = "NUMBER " + "SQUARED " + "CUBED " + "\n" + "====== " + "======= " + "===== ";
+			System.out.println(columns);
+
+			int square = 0;
+			int cube = 0;
+
+			String row = "";
+			for (int i = 1; i <= integer; i++) {
+				square = i * i;
+				cube = i * i * i;
+				row = i + "       " + square + "     " + cube;
+				System.out.println(row);
+				System.out.println();
+			}
+			System.out.print("Continue? (y/n): ");
+			choice = sc.next();
+			System.out.println();
+			
+			
+		}	while(choice.equalsIgnoreCase("y"));
+			sc.close();
+			System.out.println("Goodbye!");
+
+			}	
+		}
+	
+
+	
